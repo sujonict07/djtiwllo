7e59fbda1cf34ff89f4789dc4da736a36d5307da
sujonict07 <sujonict07@gmail.com>
2018-10-04 14:57:15 +0600

    aa

--- sms/views.py
+++ sms/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from twilio.twiml.messaging_response import MessagingResponse
 
 
+@csrf_exempt
 def sms_response(request):
-    return HttpResponse("Hello, world.")
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a text message
+    msg = resp.message("Welcome to kutumbita. How are you ?")
+
+    # body = request.values.get('Body', None)
+    print("body {}".format(type(request)))
+    print(str(resp))
+    print(request.environ)
+    return HttpResponse(str(resp))
